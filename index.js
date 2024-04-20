77e141284ec727da44f7b72bc4c5d5100f9ba9e4
Ruslan Bystranskyi <ruslanbystranskij@gmail.com>
2024-04-20 11:36:02 +0200

    Refactored

--- index.js
+++ index.js
@@ -2,19 +2,29 @@
 
 const fs = require('fs');
 
-try {
-    let aData = fs.readFileSync('./a.txt', 'utf-8');
-    let bData = fs.readFileSync('./b.txt', 'utf-8');
 
-    let a = Number(aData);
-    let b = Number(bData);
-
-    if (isNaN(a) || isNaN(b)) {
-        console.log('Type error: is not number')
-    }
-
-    console.log(a + b);
-
-} catch (err){
-    console.log(err);
+const getDataA = () => {
+    return fs.readFileSync('./a.txt', 'utf-8');
 }
+
+const getDataB = () => {
+    return fs.readFileSync('./b.txt', 'utf-8');
+}
+
+const add = (dataA, dataB) => {
+    try {
+        let a = Number(dataA)
+        let b = Number(dataB);
+
+        if (isNaN(a) || isNaN(a)) {
+            console.log('Type error: it\'s not number')
+        }
+
+        console.log(a + b);
+
+    } catch (err){
+        console.log(err);
+    }
+}
+
+add(getDataA(), getDataB());
